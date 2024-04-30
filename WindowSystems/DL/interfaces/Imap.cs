2e293b7c773f87c8d418e7beb0c7c52477dc1cd1
yehudajct <yehuda.game@gmail.com>
2024-04-30 14:14:04 +0300

    interface

---
+++ WindowSystems/DL/interfaces/Imap.cs
@@ -0,0 +1,22 @@
+﻿using WindowSystems.model.DbData;
+
+namespace WindowSystems.DL.interfaces
+{
+    public interface IMap
+    {
+        // Create operation
+        void CreateMap(Map map);
+
+        // Read operation
+        Map GetMap(double latitude, double longitude);
+
+        // Read all operation
+        List<Map> GetAllMap();
+
+        // Update operation
+        void UpdateMap(Map map);
+
+        // Delete operation
+        void DeleteMap(double latitude, double longitude);
+    }
+}
