c0fcb5a492232fd93cb7e8fe49ebf28b63d98fba
aviaMader <100083767+aviaMader@users.noreply.github.com>
2024-05-06 12:15:25 +0300

    implemention map web/sql

---
+++ WindowSystems/DL/DLImplementation/Location.cs
@@ -0,0 +1,39 @@
+﻿namespace WindowSystems.DL.DLImplementation;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WindowSystems.DL.DOApi;
+
+public class Location : ILocation
+{
+    public int Create(DO.Location entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(DO.Location entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DO.Location> Read(DO.Location entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<DO.Location> ReadAll(Func<DO.Location, bool>? func = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DO.Location ReadObject(Func<DO.Location, bool>? func)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(DO.Location entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WindowSystems/DL/WEB/WEBChatGpt.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using OpenAI_ChatGPT;
+using System.Text;
+using System.Text.Json;
+using WindowSystems.DL.DO;
+
+public class WEBChatGpt
+{
+    private readonly IChatCompletionService _chatCompletionService;
+
+    public async Task<WindowSystems.DL.DO.ChatGpt> Read(string prompt)
+    {
+        var response = await _chatCompletionService.GetChatCompletionAsync(prompt);
+        return new WindowSystems.DL.DO.ChatGpt
+        {
+            prompt = prompt,
+            responde = response
+        };
+    }
+}
+
